dddc529680aa6613fac14ef42a78123fcaad4323
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2022-05-14 19:49:58 +0530

    fix image layout

--- dotfiles.js
+++ dotfiles.js
@@ -6,5 +6,29 @@ const dotfiles = [
         description: 'My dotfiles',
         image: 'assets/bridge.png',
         tage: ['i3', 'polybar']
+    },
+    {
+        title: 'Dotfiles',
+        author: 'Armoredvortex',
+        link: 'https://github.com/armoredvortex',
+        description: 'My dotfiles',
+        image: 'assets/bridge.png',
+        tage: ['i3', 'polybar']
+    },
+    {
+        title: 'Dotfiles',
+        author: 'Armoredvortex',
+        link: 'https://github.com/armoredvortex',
+        description: 'My dotfiles',
+        image: 'assets/bridge.png',
+        tage: ['i3', 'polybar']
+    },
+    {
+        title: 'Dotfiles',
+        author: 'Armoredvortex',
+        link: 'https://github.com/armoredvortex',
+        description: 'My dotfiles',
+        image: 'assets/bridge.png',
+        tage: ['i3', 'polybar']
     }
 ]
