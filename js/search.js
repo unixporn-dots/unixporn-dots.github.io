c69baac472becb3a2e9a624f0629a27be8bc7ebf
Rahul Marban <rahulmarban@gmail.com>
2022-05-16 16:22:16 +0530

    Search result

--- js/search.js
+++ js/search.js
@@ -17,7 +17,78 @@ var fuzzy = function(items, key) {
 
 var searchByTitle = fuzzy(dotfiles, 'title');
 
+load = false;
+
 document.getElementById('searchInput').onkeyup = function() {
   result = searchByTitle(document.getElementById('searchInput').value)
- console.log(result)
+ 
+document.getElementById("themes_container").style.opacity = 0
+
+if(!load){
+ load = true;
+ 
+ setTimeout (() => {
+  document.getElementById("themes_container").innerHTML = ""
+ }, 200)
+
+setTimeout(() => {
+  document.getElementById("themes_container").style.opacity = 1
+
+ }, 1000)
+
+setTimeout(() => {
+  result.forEach((dotfile) => {
+    header = document.createElement("header");
+  
+    downld = document.createElement("a");
+    downld.classList.add("button-child");
+    downld.href = dotfile.link;
+    downld.innerHTML = "Download";
+  
+    // Desc
+    desc = document.createElement("h3");
+    desc.innerHTML = dotfile.description;
+    desc.classList.add("theme-desc");
+    header.appendChild(desc);
+  
+    // Title
+    title = document.createElement("h3");
+    title.innerHTML = dotfile.title;
+    title.classList.add("theme-title");
+    header.appendChild(title);
+  
+    // Image
+    image = document.createElement("img");
+    image.src = dotfile.image;
+  
+    // link
+    link = document.createElement("a");
+    link.href = dotfile.link;
+    link.target = "_blank";
+    header.appendChild(link);
+  
+    // link-icon
+    linkIcon = document.createElement("i");
+    linkIcon.classList.add("fas", "fa-chevron-circle-down");
+    link.appendChild(linkIcon);
+  
+    // Buttons Thingy
+    buttonz = document.createElement("div");
+    buttonz.classList.add("buttons");
+    buttonz.appendChild(downld);
+  
+    // Card
+    dotfile_div = document.createElement("div");
+    dotfile_div.classList.add("card");
+    dotfile_div.appendChild(header);
+    dotfile_div.appendChild(image);
+    dotfile_div.appendChild(buttonz);
+  
+  document.getElementById("themes_container").appendChild(dotfile_div)
+  load = false
+  });
+  }, 900)
+  
+}
+
 }
