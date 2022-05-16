351726d49e3cc4a2afa323c8937b2b85b8603517
Rahul Marban <rahulmarban@gmail.com>
2022-05-16 14:46:48 +0530

    Search

---
+++ js/search.js
@@ -0,0 +1,22 @@
+var fuzzy = function(items, key) {
+  // Returns a method that you can use to create your own reusable fuzzy search.
+
+  return function(query) {
+    var words  = query.toLowerCase().split(' ');
+
+    return items.filter(function(item) {
+      var normalizedTerm = item[key].toLowerCase();
+
+      return words.every(function(word) {
+        return (normalizedTerm.indexOf(word) > -1);
+      });
+    });
+  };
+};
+
+
+var searchByTitle = fuzzy(dotlists, 'title');
+
+document.getElementById('searchInput').onkeyup = function() {
+  result = searchByTitle(document.getElementById('searchInput').value)
+}
