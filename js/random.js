279a00d2a61c166bcd22639ac25619f852f03c46
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2022-05-16 14:13:37 +0530

    fix duplication while randomizing

--- js/random.js
+++ js/random.js
@@ -4,18 +4,71 @@ document.getElementById('js-sortSwitcher').onclick = function() {
 
 document.getElementById("themes_container").style.opacity = 0
 
-setTimeout (() => {
-  document.getElementById("themes_container").innerHTML = ""
-}, 200)
-
 if(!load){
  load = true;
+ 
+ setTimeout (() => {
+  document.getElementById("themes_container").innerHTML = ""
+ }, 200)
 
  setTimeout(() => {
   document.getElementById("themes_container").style.opacity = 1
-  load = false
 
  }, 1000)
+
+ setTimeout(() => {
+  random.forEach((dotfile) => {
+    header = document.createElement("header");
+  
+    downld = document.createElement("a");
+    downld.classList.add("button-child");
+    downld.href = dotfile.link;
+    downld.innerHTML = "Download";
+  
+    // Desc
+    desc = document.createElement("h3");
+    desc.innerHTML = dotfile.description;
+    desc.classList.add("theme-desc");
+    header.appendChild(desc);
+  
+    // Title
+    title = document.createElement("h3");
+    title.innerHTML = dotfile.title;
+    title.classList.add("theme-title");
+    header.appendChild(title);
+  
+    // Image
+    image = document.createElement("img");
+    image.src = dotfile.image;
+  
+    // link
+    link = document.createElement("a");
+    link.href = dotfile.link;
+    link.target = "_blank";
+    header.appendChild(link);
+  
+    // link-icon
+    linkIcon = document.createElement("i");
+    linkIcon.classList.add("fas", "fa-chevron-circle-down");
+    link.appendChild(linkIcon);
+  
+    // Buttons Thingy
+    buttonz = document.createElement("div");
+    buttonz.classList.add("buttons");
+    buttonz.appendChild(downld);
+  
+    // Card
+    dotfile_div = document.createElement("div");
+    dotfile_div.classList.add("card");
+    dotfile_div.appendChild(header);
+    dotfile_div.appendChild(image);
+    dotfile_div.appendChild(buttonz);
+  
+  document.getElementById("themes_container").appendChild(dotfile_div)
+  load = false
+  });
+  }, 900)
+  
 }
 
 
@@ -31,57 +84,5 @@ function randomize(sourceArray) {
     }
     return sourceArray;
 }
-setTimeout(() => {
-random.forEach((dotfile) => {
-  header = document.createElement("header");
-
-  downld = document.createElement("a");
-  downld.classList.add("button-child");
-  downld.href = dotfile.link;
-  downld.innerHTML = "Download";
-
-  // Desc
-  desc = document.createElement("h3");
-  desc.innerHTML = dotfile.description;
-  desc.classList.add("theme-desc");
-  header.appendChild(desc);
-
-  // Title
-  title = document.createElement("h3");
-  title.innerHTML = dotfile.title;
-  title.classList.add("theme-title");
-  header.appendChild(title);
-
-  // Image
-  image = document.createElement("img");
-  image.src = dotfile.image;
-
-  // link
-  link = document.createElement("a");
-  link.href = dotfile.link;
-  link.target = "_blank";
-  header.appendChild(link);
-
-  // link-icon
-  linkIcon = document.createElement("i");
-  linkIcon.classList.add("fas", "fa-chevron-circle-down");
-  link.appendChild(linkIcon);
-
-  // Buttons Thingy
-  buttonz = document.createElement("div");
-  buttonz.classList.add("buttons");
-  buttonz.appendChild(downld);
-
-  // Card
-  dotfile_div = document.createElement("div");
-  dotfile_div.classList.add("card");
-  dotfile_div.appendChild(header);
-  dotfile_div.appendChild(image);
-  dotfile_div.appendChild(buttonz);
-
-document.getElementById("themes_container").appendChild(dotfile_div)
-
-});
-}, 900)
 
 }
