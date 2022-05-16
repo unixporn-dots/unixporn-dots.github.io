a27655618fc76d36099a414bb0f3a356e93b5132
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2022-05-16 15:13:43 +0530

    first dotfiles showcase :pog:

--- dotfiles.js
+++ dotfiles.js
@@ -1,34 +1,42 @@
 const dotfiles = [
     {
-        title: 'Dotfiles-1',
-        author: 'Armoredvortex',
-        link: 'https://github.com/armoredvortex',
-        description: 'My dotfiles',
-        image: 'assets/bridge.png',
+        title: 'AmogOS',
+        author: 'Amog-OS',
+        link: 'https://github.com/Amog-OS/AmogOS',
+        description: 'A parody OS inspired by Among Us',
+        image: 'assets/AmogOS.webp',
         tags: ['i3', 'polybar']
     },
     {
-        title: 'Dotfiles-2',
-        author: 'Armoredvortex',
-        link: 'https://github.com/armoredvortex',
-        description: 'My dotfiles',
-        image: 'assets/bridge.png',
-        tags: ['i3', 'polybar']
+        title: "CodeHeister's Dotfiles",
+        author: 'CodeHeister',
+        link: 'https://github.com/CodeHeister/dotfiles',
+        description: 'First rice for i3-gaps ',
+        image: "assets/codeheister's-dotfiles.png",
+        tags: ['i3-gaps', 'rofi', 'nvim', 'ranger', 'dunst', 'flameshot']
     },
     {
-        title: 'Dotfiles-3',
-        author: 'Armoredvortex',
-        link: 'https://github.com/armoredvortex',
-        description: 'My dotfiles',
-        image: 'assets/bridge.png',
-        tags: ['i3', 'polybar']
+        title: "Elena's Dotfiles",
+        author: 'elenapan',
+        link: 'https://github.com/elenapan/dotfiles',
+        description: 'There is no place like ~/',
+        image: "assets/elena's-dotfiles.png",
+        tags: ['awesomewm', 'zsh', 'kitty', 'rofi']
     },
     {
-        title: 'Dotfiles-4',
-        author: 'Armoredvortex',
-        link: 'https://github.com/armoredvortex',
-        description: 'My dotfiles',
-        image: 'assets/bridge.png',
-        tags: ['i3', 'polybar']
+        title: "Axarva's Dotfiles",
+        author: 'Axarva',
+        link: 'https://github.com/Axarva/dotfiles-2.0',
+        description: 'XMonad. Widgets go brr.',
+        image: "assets/axarva's-dotfiles.png",
+        tags: ['xmonad', 'eww', 'rofi', 'alacritty', 'tint2']
+    },
+    {
+        title: 'Nebula',
+        author: 'Barbaross93',
+        link: 'https://github.com/Barbaross93/Nebula',
+        description: 'Desktop setup',
+        image: 'assets/nebula.png',
+        tags: ['qtile', 'alacritty', 'zsh', 'eww', 'dunst']
     }
 ]
