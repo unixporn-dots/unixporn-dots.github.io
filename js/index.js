54028d85d37556946dc108cb09988ab96c916447
Rahul Marban <rahulmarban@gmail.com>
2022-05-16 12:36:05 +0530

    Randomizer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-dotfiles.forEach((dotfile) => {
+random = randomize(dotfiles)
+
+function randomize(sourceArray) {
+
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+
+        sourceArray[j] = sourceArray[i];
+
+        sourceArray[i] = temp;
+
+    }
+
+    return sourceArray;
+
+}
+
+random.forEach((dotfile) => {
   header = document.createElement("header");
 
   downld = document.createElement("a");
