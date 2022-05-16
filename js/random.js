fb94d6b2466741288c343c9e239aa0bb46d7e489
Rahul Marban <rahulmarban@gmail.com>
2022-05-16 12:45:52 +0530

    Randomize button

---
+++ js/random.js
@@ -0,0 +1,67 @@
+document.getElementById('js-sortSwitcher').onclick = function() {
+
+document.getElementById("themes_container").innerHTML = ""
+
+random = randomize(dotfiles)
+
+function randomize(sourceArray) {
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+        sourceArray[j] = sourceArray[i];
+        sourceArray[i] = temp;
+    }
+    return sourceArray;
+}
+
+random.forEach((dotfile) => {
+  header = document.createElement("header");
+
+  downld = document.createElement("a");
+  downld.classList.add("button-child");
+  downld.href = dotfile.link;
+  downld.innerHTML = "Download";
+
+  // Desc
+  desc = document.createElement("h3");
+  desc.innerHTML = dotfile.description;
+  desc.classList.add("theme-desc");
+  header.appendChild(desc);
+
+  // Title
+  title = document.createElement("h3");
+  title.innerHTML = dotfile.title;
+  title.classList.add("theme-title");
+  header.appendChild(title);
+
+  // Image
+  image = document.createElement("img");
+  image.src = dotfile.image;
+
+  // link
+  link = document.createElement("a");
+  link.href = dotfile.link;
+  link.target = "_blank";
+  header.appendChild(link);
+
+  // link-icon
+  linkIcon = document.createElement("i");
+  linkIcon.classList.add("fas", "fa-chevron-circle-down");
+  link.appendChild(linkIcon);
+
+  // Buttons Thingy
+  buttonz = document.createElement("div");
+  buttonz.classList.add("buttons");
+  buttonz.appendChild(downld);
+
+  // Card
+  dotfile_div = document.createElement("div");
+  dotfile_div.classList.add("card");
+  dotfile_div.appendChild(header);
+  dotfile_div.appendChild(image);
+  dotfile_div.appendChild(buttonz);
+
+  document.getElementById("themes_container").appendChild(dotfile_div);
+});
+}
