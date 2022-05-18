6d880fca048fa9265208efceeb4824e838d627bc
Mahan Lamee <mahan.lameie87@gmail.com>
2022-05-18 13:40:24 +0430

    Timeout for shuffle and search button

--- js/random.js
+++ js/random.js
@@ -1,13 +1,18 @@
+var loaded = true;
 document.getElementById('js-sortSwitcher').addEventListener("click", () => {
+    if (loaded) {
+        loaded = false;
+        document.getElementById("themes_container").style.opacity = 0
+        console.log("hi");
+        random = randomize(dotfiles)
 
-    document.getElementById("themes_container").style.opacity = 0
-
-    random = randomize(dotfiles)
-
-    setTimeout(() => {
-        document.getElementById("themes_container").innerHTML = "";
-        generateCards(random);
-        document.getElementById("themes_container").style.opacity = 1;
-    }, 300);
-
+        setTimeout(() => {
+            document.getElementById("themes_container").innerHTML = "";
+            generateCards(random);
+            document.getElementById("themes_container").style.opacity = 1;
+            setTimeout(() => {
+                loaded = true;
+            }, 400);
+        }, 300);
+    }
 });
--- js/search.js
+++ js/search.js
@@ -19,33 +19,43 @@ var searchByTitle = fuzzy(dotfiles, 'title');
 var searchByTags = fuzzy(dotfiles, 'tags');
 var searchByAuthor = fuzzy(dotfiles, 'author');
 var timer;
+var loaded = true;
 var currentQuery = ""; // Handle keyup events on things like ctrl key, etc.
 
-function search (evt) {
+function search(evt) {
 
     var query = document.getElementById('searchInput').value;
 
     if (query != currentQuery || evt.currentTarget.forceSearch) { // So that we only search on query changes, not every keystroke (modifier keys, etc.)
         currentQuery = query;
-        clearTimeout(timer);
-        document.getElementById("themes_container").style.opacity = 0;
-
-        timer = setTimeout(() => {
-            document.getElementById("themes_container").innerHTML = "";
-
-            result = searchByTitle(query).concat(searchByTags(query)).concat(searchByAuthor(query));
-
-            result = new Set(result)
-            if (result.size > 0) {
-                generateCards(result);
+        if (!evt.currentTarget.forceSearch) {
+            loaded = true;
+            clearTimeout(timer);
+        }
+        if (loaded) {
+            if (evt.currentTarget.forceSearch) {
+                loaded = false;
             }
-            document.getElementById("themes_container").style.opacity = 1;
-        }, 500);
+            document.getElementById("themes_container").style.opacity = 0;
+
+            timer = setTimeout(() => {
+                document.getElementById("themes_container").innerHTML = "";
+
+                result = searchByTitle(query).concat(searchByTags(query)).concat(searchByAuthor(query));
+
+                result = new Set(result)
+                if (result.size > 0) {
+                    generateCards(result);
+                }
+                document.getElementById("themes_container").style.opacity = 1;
+                setTimeout(() => {
+                    loaded = true
+                }, 400);
+            }, 500);
+        }
     }
 
 }
-
 document.getElementById('searchInput').addEventListener('keyup', search);
 document.getElementById('searchSubmit').addEventListener('click', search);
 document.getElementById('searchSubmit').forceSearch = true;
-
