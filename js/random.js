921c982b4cea142651f319509d0aa981b8026373
Rahul Marban <rahulmarban@gmail.com>
2022-05-18 12:32:02 +0530

    Preventing shuffle spam.

--- js/random.js
+++ js/random.js
@@ -1,4 +1,9 @@
+load = false
+
 document.getElementById('js-sortSwitcher').addEventListener("click", () => {
+
+  if(load == false) {
+    load = true;
 
     document.getElementById("themes_container").style.opacity = 0
 
@@ -8,6 +13,7 @@ document.getElementById('js-sortSwitcher').addEventListener("click", () => {
         document.getElementById("themes_container").innerHTML = "";
         generateCards(random);
         document.getElementById("themes_container").style.opacity = 1;
+        load = false
     }, 300);
-
+  }
 });
