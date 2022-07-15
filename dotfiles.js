7ec7359eee6497410d2c52c21f88d75d94b5371a
Alex <adorablekitsune@pm.me>
2022-07-15 05:29:44 +0000

    Update dotfiles.js

--- dotfiles.js
+++ dotfiles.js
@@ -377,5 +377,25 @@ const dotfiles = [
     image: 'assets/alba4k.png',
     wm: 'i3-gaps',
     tags: ['i3-gaps', 'polybar']
+},
+    {
+    title: 'SAGA',
+    author: 'Miusaky',
+    link: "https://github.com/Miusaky/dotfiles",
+    reddit: 'https://www.reddit.com/r/unixporn/comments/vuaga4/dwm_saga/', // Optional
+    description: 'Dotfiles for dwm',
+    image: 'assets/SAGA_dwm.png',
+    wm: 'dwm',
+    tags: ['dwm', 'SAGA']
+},
+    {
+    title: 'SAGA',
+    author: 'Miusaky',
+    link: "https://github.com/Miusaky/dotfiles",
+    reddit: 'https://www.reddit.com/r/unixporn/comments/v95di4/bspwm_switched_to_arch/', // Optional
+    description: 'Dotfiles for bspwm',
+    image: 'assets/SAGA_bspwm.png',
+    wm: 'bspwm',
+    tags: ['bspwm', 'SAGA']
 }
 ]
