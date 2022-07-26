0df1c3018cf7291c9af6110f164ee98ad3a865c2
Rahuletto <Rahuletto@users.noreply.github.com>
2022-07-26 13:56:04 +0000

    Parse Dotfiles

--- icons.js
+++ icons.js
@@ -1,14 +1,14 @@
 const icons = [
-  {
-    author: "Rahuletto",
-    description: "An Icon to test",
-    link: "https://github.com/Rahuletto",
-    reddit: "https://www.reddit.com/r/unixporn",
-    tags: ["simple"],
-    title: "Simple Icons",
-    wm: "simplicity",
-    images: ["iconDir/Simple_Icons/thumbnail.png"],
-  },
-];
+	{
+		author: "Rahuletto",
+		description: "An Icon to test",
+		link: "https://github.com/Rahuletto",
+		reddit: "https://www.reddit.com/r/unixporn",
+		tags: ['simple'],
+		title: "Simple Icons",
+		wm: "simplicity",
+		images: ['iconDir/Simple_Icons/thumbnail.png']
+	}
+]
 
-export {icons}
+export {icons};
