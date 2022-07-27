c4897829cdd3c896bd7ed8b6e04acdfbfcb3e050
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2022-07-27 08:00:13 +0000

    Parse Dotfiles

--- icons.js
+++ icons.js
@@ -1,27 +1,19 @@
 const icons = [
 	{
+		author: "vinceliuice",
+		description: "A colorful design icon theme for linux desktops",
+		link: "https://github.com/vinceliuice/Qogir-icon-theme",
+		tags: ['colorful', 'flat'],
+		title: "Qogir Icons",
+		images: ['assets/icons/Qogir Icons/thumbnail.png', 'assets/icons/Qogir Icons/1.png']
+	},
+	{
 		author: "PapirusDevelopmentTeam",
 		description: "Papirus icon theme for Linux",
 		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
 		tags: ['material', 'vector'],
 		title: "Papirus Icon theme",
 		images: ['assets/icons/Papirus/thumbnail.png', 'assets/icons/Papirus/1.png']
-	},
-	{
-		author: "numixproject",
-		description: "Circle is an icon theme for Linux from the Numix project.",
-		link: "https://github.com/numixproject/numix-icon-theme-circle",
-		tags: ['circle', 'numix'],
-		title: "Numix Circle",
-		images: ['assets/icons/Numix Circle/thumbnail.png']
-	},
-	{
-		author: "daniruiz",
-		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
-		link: "https://github.com/daniruiz/flat-remix",
-		tags: ['material', 'gradient', 'flat', 'colorful'],
-		title: "Flat Remix",
-		images: ['assets/icons/Flat Remix/thumbnail.png']
 	},
 	{
 		author: "EliverLara",
@@ -32,12 +24,20 @@ const icons = [
 		images: ['assets/icons/Candy Icons/thumbnail.png']
 	},
 	{
-		author: "vinceliuice",
-		description: "A colorful design icon theme for linux desktops",
-		link: "https://github.com/vinceliuice/Qogir-icon-theme",
-		tags: ['colorful', 'flat'],
-		title: "Qogir Icons",
-		images: ['assets/icons/Qogir Icons/thumbnail.png', 'assets/icons/Qogir Icons/1.png']
+		author: "daniruiz",
+		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
+		link: "https://github.com/daniruiz/flat-remix",
+		tags: ['material', 'gradient', 'flat', 'colorful'],
+		title: "Flat Remix",
+		images: ['assets/icons/Flat Remix/thumbnail.png']
+	},
+	{
+		author: "numixproject",
+		description: "Circle is an icon theme for Linux from the Numix project.",
+		link: "https://github.com/numixproject/numix-icon-theme-circle",
+		tags: ['circle', 'numix'],
+		title: "Numix Circle",
+		images: ['assets/icons/Numix Circle/thumbnail.png']
 	}
 ]
 
