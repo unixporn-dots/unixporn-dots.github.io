0e01f241d4f01e90760ffa287dfc6d6a8b441351
Rahul <rahulmarban@gmail.com>
2022-07-28 17:22:18 +0530

    test run py and fixed and works

--- gtk-themes.js
+++ gtk-themes.js
@@ -1,24 +1,20 @@
 const gtkthemes = [
-  {
-    author: "Vinceliuice",
-    description:
-      "Orchis is a Material Design theme for GNOME/GTK based desktop environments. Based on nana-4",
-    link: "https://github.com/vinceliuice/Orchis-theme",
-    tags: ["minimal", "material", "gnome"],
-    title: "Orchis theme",
-    images: ["assets/gtk-themes/Orchis theme/thumbnail.png"],
-  },
-  {
-    author: "Jannomag",
-    description: "A fork of Ubuntu's Yaru theme - in different colors",
-    link: "https://github.com/Jannomag/Yaru-Colors",
-    tags: ["colors", "minimal"],
-    title: "Yaru-Colors",
-    images: [
-      "assets/gtk-themes/Yaru-Colors/thumbnail.png",
-      "assets/gtk-themes/Yaru-Colors/1.png",
-    ],
-  },
-];
+	{
+		title: "Dracula",
+		images: ['assets/gtk-themes/Dracula/thumbnail.png']
+	},
+	{
+		title: "Mojave",
+		images: ['assets/gtk-themes/Mojave/thumbnail.jpeg']
+	},
+	{
+		title: "Orchis theme",
+		images: ['assets/gtk-themes/Orchis theme/thumbnail.png']
+	},
+	{
+		title: "Yaru-Colors",
+		images: ['assets/gtk-themes/Yaru-Colors/thumbnail.png', 'assets/gtk-themes/Yaru-Colors/1.png']
+	}
+]
 
-export { gtkthemes };
+export {gtkthemes};
