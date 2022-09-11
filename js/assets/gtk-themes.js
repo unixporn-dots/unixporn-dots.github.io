e76f41a118828d2836200a5d5bf75717819c2907
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2022-09-11 07:50:45 +0000

    Parse Dotfiles

--- js/assets/gtk-themes.js
+++ js/assets/gtk-themes.js
@@ -1,11 +1,19 @@
 const gtkthemes = [
 	{
+		author: "Eliver Lara",
+		description: "Dark theme for GTK",
+		link: "https://github.com/dracula/gtk",
+		tags: ['dark', 'dracula', 'minimal'],
+		title: "Dracula",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/gtk-themes/Dracula/thumbnail.png']
+	},
+	{
 		author: "vinceliuice",
 		description: "Mojave is a macos Mojave like theme for GTK 3, GTK 2 and Gnome-Shell",
 		link: "https://github.com/vinceliuice/Mojave-gtk-theme",
 		tags: ['macos', 'round'],
 		title: "Mojave",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/gtk-themes/Mojave/thumbnail.jpeg']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/gtk-themes/Mojave/thumbnail.jpeg']
 	},
 	{
 		author: "Vinceliuice",
@@ -13,7 +21,7 @@ const gtkthemes = [
 		link: "https://github.com/vinceliuice/Orchis-theme",
 		tags: ['minimal', 'material', 'gnome'],
 		title: "Orchis theme",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/gtk-themes/Orchis theme/thumbnail.png']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/gtk-themes/Orchis theme/thumbnail.png']
 	},
 	{
 		author: "Jannomag",
@@ -21,15 +29,7 @@ const gtkthemes = [
 		link: "https://github.com/Jannomag/Yaru-Colors",
 		tags: ['colors', 'minimal'],
 		title: "Yaru-Colors",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/gtk-themes/Yaru-Colors/thumbnail.png', '/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/gtk-themes/Yaru-Colors/1.png']
-	},
-	{
-		author: "Eliver Lara",
-		description: "Dark theme for GTK",
-		link: "https://github.com/dracula/gtk",
-		tags: ['dark', 'dracula', 'minimal'],
-		title: "Dracula",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/gtk-themes/Dracula/thumbnail.png']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/gtk-themes/Yaru-Colors/thumbnail.png', '/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/gtk-themes/Yaru-Colors/1.png']
 	}
 ]
 
--- js/assets/icons.js
+++ js/assets/icons.js
@@ -1,67 +1,11 @@
 const icons = [
 	{
-		author: "EliverLara",
-		description: "Sweet gradient icons",
-		link: "https://github.com/EliverLara/candy-icons",
-		tags: ['candy', 'minimal', 'gradient'],
-		title: "Candy Icons",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Candy Icons/thumbnail.png']
-	},
-	{
-		author: "madmaxms",
-		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
-		link: "https://github.com/madmaxms/iconpack-obsidian",
-		tags: ['dark', 'gnome'],
-		title: "Obsidian",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Obsidian/thumbnail.png', '/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Obsidian/1.png']
-	},
-	{
-		author: "vinceliuice",
-		description: "A colorful design icon theme for linux desktops",
-		link: "https://github.com/vinceliuice/Qogir-icon-theme",
-		tags: ['colorful', 'flat'],
-		title: "Qogir Icons",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Qogir Icons/thumbnail.png', '/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Qogir Icons/1.png', '/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Qogir Icons/2.png']
-	},
-	{
 		author: "cbrnix",
 		description: "Linux icon theme",
 		link: "https://github.com/cbrnix/Newaita",
 		tags: ['cartoony'],
 		title: "Newaita",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Newaita/thumbnail.png']
-	},
-	{
-		author: "OrancheloTeam",
-		description: "Oranchelo icon theme",
-		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
-		tags: ['orange'],
-		title: "Oranchelo Icons",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Oranchelo Icons/thumbnail.png']
-	},
-	{
-		author: "ju1464",
-		description: "Monochrome icons with some brush strokes of color.",
-		link: "https://github.com/ju1464/Simply_Circles_Icons",
-		tags: ['circles', 'color'],
-		title: "Simply Circles",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Simply Circles/thumbnail.png']
-	},
-	{
-		author: "numixproject",
-		description: "Circle is an icon theme for Linux from the Numix project.",
-		link: "https://github.com/numixproject/numix-icon-theme-circle",
-		tags: ['circle', 'numix'],
-		title: "Numix Circle",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Numix Circle/thumbnail.png']
-	},
-	{
-		author: "daniruiz",
-		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
-		link: "https://github.com/daniruiz/flat-remix",
-		tags: ['material', 'gradient', 'flat', 'colorful'],
-		title: "Flat Remix",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Flat Remix/thumbnail.png']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Newaita/thumbnail.png']
 	},
 	{
 		author: "PapirusDevelopmentTeam",
@@ -69,7 +13,15 @@ const icons = [
 		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
 		tags: ['material', 'vector'],
 		title: "Papirus Icon theme",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Papirus/thumbnail.png', '/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Papirus/1.png']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Papirus/thumbnail.png', '/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Papirus/1.png']
+	},
+	{
+		author: "ju1464",
+		description: "Monochrome icons with some brush strokes of color.",
+		link: "https://github.com/ju1464/Simply_Circles_Icons",
+		tags: ['circles', 'color'],
+		title: "Simply Circles",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Simply Circles/thumbnail.png']
 	},
 	{
 		author: "franksouza183",
@@ -77,7 +29,55 @@ const icons = [
 		link: "https://github.com/franksouza183/Evolvere-Icons",
 		tags: ['realism', 'gnu'],
 		title: "Evolvere Icons",
-		images: ['/home/francesco/Documents/Projects/Code/unixporn-dots.github.io/assets/icons/Evolvere Icons/thumbnail.png']
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Evolvere Icons/thumbnail.png']
+	},
+	{
+		author: "EliverLara",
+		description: "Sweet gradient icons",
+		link: "https://github.com/EliverLara/candy-icons",
+		tags: ['candy', 'minimal', 'gradient'],
+		title: "Candy Icons",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Candy Icons/thumbnail.png']
+	},
+	{
+		author: "madmaxms",
+		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
+		link: "https://github.com/madmaxms/iconpack-obsidian",
+		tags: ['dark', 'gnome'],
+		title: "Obsidian",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Obsidian/thumbnail.png', '/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Obsidian/1.png']
+	},
+	{
+		author: "vinceliuice",
+		description: "A colorful design icon theme for linux desktops",
+		link: "https://github.com/vinceliuice/Qogir-icon-theme",
+		tags: ['colorful', 'flat'],
+		title: "Qogir Icons",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Qogir Icons/thumbnail.png', '/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Qogir Icons/1.png', '/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Qogir Icons/2.png']
+	},
+	{
+		author: "OrancheloTeam",
+		description: "Oranchelo icon theme",
+		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
+		tags: ['orange'],
+		title: "Oranchelo Icons",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Oranchelo Icons/thumbnail.png']
+	},
+	{
+		author: "daniruiz",
+		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
+		link: "https://github.com/daniruiz/flat-remix",
+		tags: ['material', 'gradient', 'flat', 'colorful'],
+		title: "Flat Remix",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Flat Remix/thumbnail.png']
+	},
+	{
+		author: "numixproject",
+		description: "Circle is an icon theme for Linux from the Numix project.",
+		link: "https://github.com/numixproject/numix-icon-theme-circle",
+		tags: ['circle', 'numix'],
+		title: "Numix Circle",
+		images: ['/home/runner/work/unixporn-dots.github.io/unixporn-dots.github.io/assets/icons/Numix Circle/thumbnail.png']
 	}
 ]
 
