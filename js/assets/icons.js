7aa9bdb920c723df387fc8eb6645e56cf94fe9f5
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2022-10-05 06:51:48 +0000

    Parse Dotfiles

--- js/assets/icons.js
+++ js/assets/icons.js
@@ -8,28 +8,12 @@ const icons = [
 		images: ['./assets/icons/Newaita/thumbnail.png']
 	},
 	{
-		author: "PapirusDevelopmentTeam",
-		description: "Papirus icon theme for Linux",
-		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
-		tags: ['material', 'vector'],
-		title: "Papirus Icon theme",
-		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
-	},
-	{
-		author: "ju1464",
-		description: "Monochrome icons with some brush strokes of color.",
-		link: "https://github.com/ju1464/Simply_Circles_Icons",
-		tags: ['circles', 'color'],
-		title: "Simply Circles",
-		images: ['./assets/icons/Simply Circles/thumbnail.png']
-	},
-	{
-		author: "franksouza183",
-		description: "A linux icon theme, part of Evolvere Suit",
-		link: "https://github.com/franksouza183/Evolvere-Icons",
-		tags: ['realism', 'gnu'],
-		title: "Evolvere Icons",
-		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
+		author: "daniruiz",
+		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
+		link: "https://github.com/daniruiz/flat-remix",
+		tags: ['material', 'gradient', 'flat', 'colorful'],
+		title: "Flat Remix",
+		images: ['./assets/icons/Flat Remix/thumbnail.png']
 	},
 	{
 		author: "EliverLara",
@@ -56,20 +40,36 @@ const icons = [
 		images: ['./assets/icons/Qogir Icons/thumbnail.png', './assets/icons/Qogir Icons/1.png', './assets/icons/Qogir Icons/2.png']
 	},
 	{
+		author: "franksouza183",
+		description: "A linux icon theme, part of Evolvere Suit",
+		link: "https://github.com/franksouza183/Evolvere-Icons",
+		tags: ['realism', 'gnu'],
+		title: "Evolvere Icons",
+		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
+	},
+	{
+		author: "PapirusDevelopmentTeam",
+		description: "Papirus icon theme for Linux",
+		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
+		tags: ['material', 'vector'],
+		title: "Papirus Icon theme",
+		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
+	},
+	{
+		author: "ju1464",
+		description: "Monochrome icons with some brush strokes of color.",
+		link: "https://github.com/ju1464/Simply_Circles_Icons",
+		tags: ['circles', 'color'],
+		title: "Simply Circles",
+		images: ['./assets/icons/Simply Circles/thumbnail.png']
+	},
+	{
 		author: "OrancheloTeam",
 		description: "Oranchelo icon theme",
 		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
 		tags: ['orange'],
 		title: "Oranchelo Icons",
 		images: ['./assets/icons/Oranchelo Icons/thumbnail.png']
-	},
-	{
-		author: "daniruiz",
-		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
-		link: "https://github.com/daniruiz/flat-remix",
-		tags: ['material', 'gradient', 'flat', 'colorful'],
-		title: "Flat Remix",
-		images: ['./assets/icons/Flat Remix/thumbnail.png']
 	},
 	{
 		author: "numixproject",
