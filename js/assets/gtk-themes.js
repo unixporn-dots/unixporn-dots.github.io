fc90197b35b0cb40ecbf801214516fc90bb577d8
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2022-12-19 11:42:26 +0000

    Parse Dotfiles

--- js/assets/gtk-themes.js
+++ js/assets/gtk-themes.js
@@ -1,11 +1,11 @@
 const gtkthemes = [
 	{
-		author: "Jannomag",
-		description: "A fork of Ubuntu\'s Yaru theme - in different colors",
-		link: "https://github.com/Jannomag/Yaru-Colors",
-		tags: ['colors', 'minimal'],
-		title: "Yaru-Colors",
-		images: ['./assets/gtk-themes/Yaru-Colors/thumbnail.png', './assets/gtk-themes/Yaru-Colors/1.png']
+		author: "Vinceliuice",
+		description: "Orchis is a Material Design theme for GNOME/GTK based desktop environments. Based on nana-4",
+		link: "https://github.com/vinceliuice/Orchis-theme",
+		tags: ['minimal', 'material', 'gnome'],
+		title: "Orchis theme",
+		images: ['./assets/gtk-themes/Orchis theme/thumbnail.png']
 	},
 	{
 		author: "vinceliuice",
@@ -16,20 +16,20 @@ const gtkthemes = [
 		images: ['./assets/gtk-themes/Mojave/thumbnail.jpeg']
 	},
 	{
-		author: "Vinceliuice",
-		description: "Orchis is a Material Design theme for GNOME/GTK based desktop environments. Based on nana-4",
-		link: "https://github.com/vinceliuice/Orchis-theme",
-		tags: ['minimal', 'material', 'gnome'],
-		title: "Orchis theme",
-		images: ['./assets/gtk-themes/Orchis theme/thumbnail.png']
-	},
-	{
 		author: "Eliver Lara",
 		description: "Dark theme for GTK",
 		link: "https://github.com/dracula/gtk",
 		tags: ['dark', 'dracula', 'minimal'],
 		title: "Dracula",
 		images: ['./assets/gtk-themes/Dracula/thumbnail.png']
+	},
+	{
+		author: "Jannomag",
+		description: "A fork of Ubuntu\'s Yaru theme - in different colors",
+		link: "https://github.com/Jannomag/Yaru-Colors",
+		tags: ['colors', 'minimal'],
+		title: "Yaru-Colors",
+		images: ['./assets/gtk-themes/Yaru-Colors/thumbnail.png', './assets/gtk-themes/Yaru-Colors/1.png']
 	}
 ]
 
