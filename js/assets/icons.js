6cbd9d90871e17fa80a7bb0e7253395898fd48e7
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2024-03-14 08:06:23 +0000

    Parse Dotfiles

--- js/assets/icons.js
+++ js/assets/icons.js
@@ -1,27 +1,11 @@
 const icons = [
 	{
-		author: "EliverLara",
-		description: "Sweet gradient icons",
-		link: "https://github.com/EliverLara/candy-icons",
-		tags: ['candy', 'minimal', 'gradient'],
-		title: "Candy Icons",
-		images: ['./assets/icons/Candy Icons/thumbnail.png']
-	},
-	{
-		author: "franksouza183",
-		description: "A linux icon theme, part of Evolvere Suit",
-		link: "https://github.com/franksouza183/Evolvere-Icons",
-		tags: ['realism', 'gnu'],
-		title: "Evolvere Icons",
-		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
-	},
-	{
-		author: "daniruiz",
-		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
-		link: "https://github.com/daniruiz/flat-remix",
-		tags: ['material', 'gradient', 'flat', 'colorful'],
-		title: "Flat Remix",
-		images: ['./assets/icons/Flat Remix/thumbnail.png']
+		author: "cbrnix",
+		description: "Linux icon theme",
+		link: "https://github.com/cbrnix/Newaita",
+		tags: ['cartoony'],
+		title: "Newaita",
+		images: ['./assets/icons/Newaita/thumbnail.png']
 	},
 	{
 		author: "ju1464",
@@ -32,36 +16,20 @@ const icons = [
 		images: ['./assets/icons/Simply Circles/thumbnail.png']
 	},
 	{
-		author: "Frostbitten-jello",
-		description: "A skeuomorphic take on adwaita theme.",
-		link: "https://github.com/Frostbitten-jello/Skeuowaita",
-		tags: ['skeumorphic', 'Adwaita'],
-		title: "Skeuowaita",
-		images: ['./assets/icons/Skeuowaita/thumbnail.webp']
+		author: "franksouza183",
+		description: "A linux icon theme, part of Evolvere Suit",
+		link: "https://github.com/franksouza183/Evolvere-Icons",
+		tags: ['realism', 'gnu'],
+		title: "Evolvere Icons",
+		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
 	},
 	{
-		author: "cbrnix",
-		description: "Linux icon theme",
-		link: "https://github.com/cbrnix/Newaita",
-		tags: ['cartoony'],
-		title: "Newaita",
-		images: ['./assets/icons/Newaita/thumbnail.png']
-	},
-	{
-		author: "madmaxms",
-		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
-		link: "https://github.com/madmaxms/iconpack-obsidian",
-		tags: ['dark', 'gnome'],
-		title: "Obsidian",
-		images: ['./assets/icons/Obsidian/thumbnail.png', './assets/icons/Obsidian/1.png']
-	},
-	{
-		author: "PapirusDevelopmentTeam",
-		description: "Papirus icon theme for Linux",
-		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
-		tags: ['material', 'vector'],
-		title: "Papirus Icon theme",
-		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
+		author: "OrancheloTeam",
+		description: "Oranchelo icon theme",
+		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
+		tags: ['orange'],
+		title: "Oranchelo Icons",
+		images: ['./assets/icons/Oranchelo Icons/thumbnail.png']
 	},
 	{
 		author: "numixproject",
@@ -80,12 +48,44 @@ const icons = [
 		images: ['./assets/icons/Qogir Icons/thumbnail.png', './assets/icons/Qogir Icons/1.png', './assets/icons/Qogir Icons/2.png']
 	},
 	{
-		author: "OrancheloTeam",
-		description: "Oranchelo icon theme",
-		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
-		tags: ['orange'],
-		title: "Oranchelo Icons",
-		images: ['./assets/icons/Oranchelo Icons/thumbnail.png']
+		author: "EliverLara",
+		description: "Sweet gradient icons",
+		link: "https://github.com/EliverLara/candy-icons",
+		tags: ['candy', 'minimal', 'gradient'],
+		title: "Candy Icons",
+		images: ['./assets/icons/Candy Icons/thumbnail.png']
+	},
+	{
+		author: "madmaxms",
+		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
+		link: "https://github.com/madmaxms/iconpack-obsidian",
+		tags: ['dark', 'gnome'],
+		title: "Obsidian",
+		images: ['./assets/icons/Obsidian/thumbnail.png', './assets/icons/Obsidian/1.png']
+	},
+	{
+		author: "Frostbitten-jello",
+		description: "A skeuomorphic take on adwaita theme.",
+		link: "https://github.com/Frostbitten-jello/Skeuowaita",
+		tags: ['skeumorphic', 'Adwaita'],
+		title: "Skeuowaita",
+		images: ['./assets/icons/Skeuowaita/thumbnail.webp']
+	},
+	{
+		author: "PapirusDevelopmentTeam",
+		description: "Papirus icon theme for Linux",
+		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
+		tags: ['material', 'vector'],
+		title: "Papirus Icon theme",
+		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
+	},
+	{
+		author: "daniruiz",
+		description: "Flat Remix is an icon theme inspired by material design. It is mostly flat using a colorful palette with some shadows, highlights, and gradients for some depth.",
+		link: "https://github.com/daniruiz/flat-remix",
+		tags: ['material', 'gradient', 'flat', 'colorful'],
+		title: "Flat Remix",
+		images: ['./assets/icons/Flat Remix/thumbnail.png']
 	}
 ]
 
