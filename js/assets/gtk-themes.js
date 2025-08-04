e46e899701776d94d98b47d97095b8a778931b04
ArmoredVortex <ArmoredVortex@users.noreply.github.com>
2025-08-04 08:55:34 +0000

    Parse Dotfiles

--- js/assets/gtk-themes.js
+++ js/assets/gtk-themes.js
@@ -1,19 +1,19 @@
 const gtkthemes = [
 	{
-		author: "vinceliuice",
-		description: "Mojave is a macos Mojave like theme for GTK 3, GTK 2 and Gnome-Shell",
-		link: "https://github.com/vinceliuice/Mojave-gtk-theme",
-		tags: ['macos', 'round'],
-		title: "Mojave",
-		images: ['./assets/gtk-themes/Mojave/thumbnail.jpeg']
-	},
-	{
 		author: "Vinceliuice",
 		description: "Orchis is a Material Design theme for GNOME/GTK based desktop environments. Based on nana-4",
 		link: "https://github.com/vinceliuice/Orchis-theme",
 		tags: ['minimal', 'material', 'gnome'],
 		title: "Orchis theme",
 		images: ['./assets/gtk-themes/Orchis theme/thumbnail.png']
+	},
+	{
+		author: "vinceliuice",
+		description: "Mojave is a macos Mojave like theme for GTK 3, GTK 2 and Gnome-Shell",
+		link: "https://github.com/vinceliuice/Mojave-gtk-theme",
+		tags: ['macos', 'round'],
+		title: "Mojave",
+		images: ['./assets/gtk-themes/Mojave/thumbnail.jpeg']
 	},
 	{
 		author: "Eliver Lara",
--- js/assets/icons.js
+++ js/assets/icons.js
@@ -1,11 +1,11 @@
 const icons = [
 	{
-		author: "cbrnix",
-		description: "Linux icon theme",
-		link: "https://github.com/cbrnix/Newaita",
-		tags: ['cartoony'],
-		title: "Newaita",
-		images: ['./assets/icons/Newaita/thumbnail.png']
+		author: "franksouza183",
+		description: "A linux icon theme, part of Evolvere Suit",
+		link: "https://github.com/franksouza183/Evolvere-Icons",
+		tags: ['realism', 'gnu'],
+		title: "Evolvere Icons",
+		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
 	},
 	{
 		author: "ju1464",
@@ -16,52 +16,12 @@ const icons = [
 		images: ['./assets/icons/Simply Circles/thumbnail.png']
 	},
 	{
-		author: "franksouza183",
-		description: "A linux icon theme, part of Evolvere Suit",
-		link: "https://github.com/franksouza183/Evolvere-Icons",
-		tags: ['realism', 'gnu'],
-		title: "Evolvere Icons",
-		images: ['./assets/icons/Evolvere Icons/thumbnail.png']
-	},
-	{
-		author: "OrancheloTeam",
-		description: "Oranchelo icon theme",
-		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
-		tags: ['orange'],
-		title: "Oranchelo Icons",
-		images: ['./assets/icons/Oranchelo Icons/thumbnail.png']
-	},
-	{
 		author: "numixproject",
 		description: "Circle is an icon theme for Linux from the Numix project.",
 		link: "https://github.com/numixproject/numix-icon-theme-circle",
 		tags: ['circle', 'numix'],
 		title: "Numix Circle",
 		images: ['./assets/icons/Numix Circle/thumbnail.png']
-	},
-	{
-		author: "vinceliuice",
-		description: "A colorful design icon theme for linux desktops",
-		link: "https://github.com/vinceliuice/Qogir-icon-theme",
-		tags: ['colorful', 'flat'],
-		title: "Qogir Icons",
-		images: ['./assets/icons/Qogir Icons/thumbnail.png', './assets/icons/Qogir Icons/1.png', './assets/icons/Qogir Icons/2.png']
-	},
-	{
-		author: "EliverLara",
-		description: "Sweet gradient icons",
-		link: "https://github.com/EliverLara/candy-icons",
-		tags: ['candy', 'minimal', 'gradient'],
-		title: "Candy Icons",
-		images: ['./assets/icons/Candy Icons/thumbnail.png']
-	},
-	{
-		author: "madmaxms",
-		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
-		link: "https://github.com/madmaxms/iconpack-obsidian",
-		tags: ['dark', 'gnome'],
-		title: "Obsidian",
-		images: ['./assets/icons/Obsidian/thumbnail.png', './assets/icons/Obsidian/1.png']
 	},
 	{
 		author: "Frostbitten-jello",
@@ -72,12 +32,36 @@ const icons = [
 		images: ['./assets/icons/Skeuowaita/thumbnail.webp']
 	},
 	{
-		author: "PapirusDevelopmentTeam",
-		description: "Papirus icon theme for Linux",
-		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
-		tags: ['material', 'vector'],
-		title: "Papirus Icon theme",
-		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
+		author: "madmaxms",
+		description: "Gnome Icon Pack based upon Faenza, optimized for dark themes",
+		link: "https://github.com/madmaxms/iconpack-obsidian",
+		tags: ['dark', 'gnome'],
+		title: "Obsidian",
+		images: ['./assets/icons/Obsidian/thumbnail.png', './assets/icons/Obsidian/1.png']
+	},
+	{
+		author: "cbrnix",
+		description: "Linux icon theme",
+		link: "https://github.com/cbrnix/Newaita",
+		tags: ['cartoony'],
+		title: "Newaita",
+		images: ['./assets/icons/Newaita/thumbnail.png']
+	},
+	{
+		author: "OrancheloTeam",
+		description: "Oranchelo icon theme",
+		link: "https://github.com/OrancheloTeam/oranchelo-icon-theme",
+		tags: ['orange'],
+		title: "Oranchelo Icons",
+		images: ['./assets/icons/Oranchelo Icons/thumbnail.png']
+	},
+	{
+		author: "EliverLara",
+		description: "Sweet gradient icons",
+		link: "https://github.com/EliverLara/candy-icons",
+		tags: ['candy', 'minimal', 'gradient'],
+		title: "Candy Icons",
+		images: ['./assets/icons/Candy Icons/thumbnail.png']
 	},
 	{
 		author: "daniruiz",
@@ -86,6 +70,22 @@ const icons = [
 		tags: ['material', 'gradient', 'flat', 'colorful'],
 		title: "Flat Remix",
 		images: ['./assets/icons/Flat Remix/thumbnail.png']
+	},
+	{
+		author: "vinceliuice",
+		description: "A colorful design icon theme for linux desktops",
+		link: "https://github.com/vinceliuice/Qogir-icon-theme",
+		tags: ['colorful', 'flat'],
+		title: "Qogir Icons",
+		images: ['./assets/icons/Qogir Icons/thumbnail.png', './assets/icons/Qogir Icons/1.png', './assets/icons/Qogir Icons/2.png']
+	},
+	{
+		author: "PapirusDevelopmentTeam",
+		description: "Papirus icon theme for Linux",
+		link: "https://github.com/PapirusDevelopmentTeam/papirus-icon-theme",
+		tags: ['material', 'vector'],
+		title: "Papirus Icon theme",
+		images: ['./assets/icons/Papirus/thumbnail.png', './assets/icons/Papirus/1.png']
 	}
 ]
 
