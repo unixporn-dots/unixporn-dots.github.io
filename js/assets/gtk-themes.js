76ce49267f6c0d598d2ca19586f7abbee8945098
armoredvortex <armoredvortex@users.noreply.github.com>
2026-06-04 19:03:54 +0000

    Parse Dotfiles

--- js/assets/gtk-themes.js
+++ js/assets/gtk-themes.js
@@ -1,5 +1,13 @@
 const gtkthemes = [
 	{
+		author: "Jannomag",
+		description: "A fork of Ubuntu\'s Yaru theme - in different colors",
+		link: "https://github.com/Jannomag/Yaru-Colors",
+		tags: ['colors', 'minimal'],
+		title: "Yaru-Colors",
+		images: ['./assets/gtk-themes/Yaru-Colors/thumbnail.png', './assets/gtk-themes/Yaru-Colors/1.png']
+	},
+	{
 		author: "Vinceliuice",
 		description: "Orchis is a Material Design theme for GNOME/GTK based desktop environments. Based on nana-4",
 		link: "https://github.com/vinceliuice/Orchis-theme",
@@ -14,14 +22,6 @@ const gtkthemes = [
 		tags: ['macos', 'round'],
 		title: "Mojave",
 		images: ['./assets/gtk-themes/Mojave/thumbnail.jpeg']
-	},
-	{
-		author: "Jannomag",
-		description: "A fork of Ubuntu\'s Yaru theme - in different colors",
-		link: "https://github.com/Jannomag/Yaru-Colors",
-		tags: ['colors', 'minimal'],
-		title: "Yaru-Colors",
-		images: ['./assets/gtk-themes/Yaru-Colors/thumbnail.png', './assets/gtk-themes/Yaru-Colors/1.png']
 	},
 	{
 		author: "Eliver Lara",
